fbf980d7180adf40892ca25942c4563ae65e8a26
Nick <nrsantos@ucdavis.edu>
2018-09-07 20:17:30 -0700

    New run to correct taxonomic uniqueness issue

--- scripts/create_clusters_phase2.py
+++ scripts/create_clusters_phase2.py
@@ -37,7 +37,7 @@ eflows_folder = os.path.split(os.path.split(os.path.abspath(__file__))[0])[0]  #
 #	metadata.finish()
 
 aggregation_level = "species"
-delta_practice = os.path.join(eflows_folder, r"data\report_update\scratch.gdb\nodelta_noempty_{}_aggregation_v2_2018_09_06".format(aggregation_level))
+delta_practice = os.path.join(eflows_folder, r"data\report_update\scratch.gdb\nodelta_noempty_{}_aggregation_v3_2018_09_06".format(aggregation_level))
 presence_types = "1,3,9"
 tbx_make_clusters.make_species_clusters(output_path=delta_practice,
 										group_name="Flow_Sensitive",
@@ -51,7 +51,7 @@ tbx_make_clusters.make_species_clusters(output_path=delta_practice,
 
 
 metadata = arcpy_metadata.MetadataEditor(delta_practice)
-metadata.purpose.set("Clustering of flow sensitive species aggregated to {} level for PISCES presence values {} using spatial constraint contiguity edges corners. New clustering run to remove Delta per group meeting. Also removing out of state areas (and anywhere else that has no species data in PISCES or no species at all - we won't cluster empty spaces on this run). Adding 2 group run for great basin. Run by Nick 9/6/18".format(presence_types,aggregation_level))
+metadata.purpose = "Clustering of flow sensitive species aggregated to {} level for PISCES presence values {} using spatial constraint contiguity edges corners. New clustering run to remove Delta per group meeting. Also removing out of state areas (and anywhere else that has no species data in PISCES or no species at all - we won't cluster empty spaces on this run). Adding 2 group run for great basin. Newest version should feature truly unique taxonomic tree aggregation (because it uses family, genus, and species instead of just whichever attribute, which can be (and sometimes is) a duplicate. Run by Nick 9/6/18".format(presence_types,aggregation_level)
 metadata.finish()
 
 
