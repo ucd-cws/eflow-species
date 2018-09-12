7dbc867adca67436f05262af3a83853d9558ec93
Nick <nrsantos@ucdavis.edu>
2018-09-12 12:40:39 -0700

    Updated clustering script (clusters still missing) and updated species aggregation lookup table

--- scripts/create_clusters_phase2.py
+++ scripts/create_clusters_phase2.py
@@ -36,8 +36,26 @@ eflows_folder = os.path.split(os.path.split(os.path.abspath(__file__))[0])[0]  #
 #	metadata.purpose.set("Clustering of flow sensitive species aggregated to {} level for PISCES presence values {} using spatial constraint contiguity edges corners. New clustering run to remove Delta per group meeting. Run by Nick 8/29/18".format(presence_types,aggregation_level))
 #	metadata.finish()
 
+# aggregation_level = "species"
+# delta_practice = os.path.join(eflows_folder, r"data\report_update\scratch.gdb\nodelta_noempty_{}_aggregation_v3_2018_09_06".format(aggregation_level))
+# presence_types = "1,3,9"
+# tbx_make_clusters.make_species_clusters(output_path=delta_practice,
+# 										group_name="Flow_Sensitive",
+# 										presence_values=presence_types,  # current presence without translocations
+# 										min_species=1,  # this is a shortcut to remove all the out of state areas - don't cluster empty areas
+# 										num_groups=(2, 3, 4, 5, 6, 7, 8),  # added two because great_basin region is especially wonky - see what happens with only two groups
+# 										huc_regions=os.path.join(eflows_folder, r"data\report_update\regions_new.gdb\regions_wo_desert_delta"),
+# 										region_group_field="huc_region_group",
+# 										aggregation=aggregation_level,
+# 										spatial_constraint="CONTIGUITY_EDGES_CORNERS")
+
+
+# metadata = arcpy_metadata.MetadataEditor(delta_practice)
+# metadata.purpose = "Clustering of flow sensitive species aggregated to {} level for PISCES presence values {} using spatial constraint contiguity edges corners. New clustering run to remove Delta per group meeting. Also removing out of state areas (and anywhere else that has no species data in PISCES or no species at all - we won't cluster empty spaces on this run). Adding 2 group run for great basin. Newest version should feature truly unique taxonomic tree aggregation (because it uses family, genus, and species instead of just whichever attribute, which can be (and sometimes is) a duplicate. Run by Nick 9/6/18".format(presence_types,aggregation_level)
+# metadata.finish()
+
 aggregation_level = "species"
-delta_practice = os.path.join(eflows_folder, r"data\report_update\scratch.gdb\nodelta_noempty_{}_aggregation_v3_2018_09_06".format(aggregation_level))
+delta_practice = os.path.join(eflows_folder, r"data\report_update\scratch.gdb\nodelta_noempty_{}_aggregation_v4_2018_09_12".format(aggregation_level))
 presence_types = "1,3,9"
 tbx_make_clusters.make_species_clusters(output_path=delta_practice,
 										group_name="Flow_Sensitive",
@@ -51,7 +69,5 @@ tbx_make_clusters.make_species_clusters(output_path=delta_practice,
 
 
 metadata = arcpy_metadata.MetadataEditor(delta_practice)
-metadata.purpose = "Clustering of flow sensitive species aggregated to {} level for PISCES presence values {} using spatial constraint contiguity edges corners. New clustering run to remove Delta per group meeting. Also removing out of state areas (and anywhere else that has no species data in PISCES or no species at all - we won't cluster empty spaces on this run). Adding 2 group run for great basin. Newest version should feature truly unique taxonomic tree aggregation (because it uses family, genus, and species instead of just whichever attribute, which can be (and sometimes is) a duplicate. Run by Nick 9/6/18".format(presence_types,aggregation_level)
+metadata.purpose = "Clustering of flow sensitive species aggregated to {} level for PISCES presence values {} using spatial constraint contiguity edges corners. New clustering run to remove Delta per group meeting. Also removing out of state areas (and anywhere else that has no species data in PISCES or no species at all - we won't cluster empty spaces on this run). Adding 2 group run for great basin. Newest version should feature truly unique taxonomic tree aggregation (because it uses family, genus, and species instead of just whichever attribute, which can be (and sometimes is) a duplicate. This version also reflects taxonomic changes and corrections in PISCES, specifically to Roach subspecies and to the split between Tidewater Goby. Run by Nick 9/12/18".format(presence_types,aggregation_level)
 metadata.finish()
-
-
