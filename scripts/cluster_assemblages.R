1aa7bb48c16cdb70954282ecc15ba974262004bb
Nick <nrsantos@ucdavis.edu>
2018-10-05 17:28:16 -0700

    Tables for fancy map and supporting pieces

--- scripts/cluster_assemblages.R
+++ scripts/cluster_assemblages.R
@@ -6,72 +6,43 @@ library(dplyr)
 library(tidyr)
 
 
-clustering_results <- read_excel("C:/Users/aobester/Desktop/clustering_results_10042018.xls") #load the data
+clustering_data <- read.csv("data/web/fancy_map_data_base.csv") #load the data
 
 #break up data into 4 regions and then want assemblage list for each cluster within a region
 
 
 
+get_cluster_assemblages <- function(clustering_results, region_group){
+  filtered <- filter(clustering_results, huc_region_group == region_group) 
+  
+  separated <- separate_rows(filtered, current_assemblage_Flow_Sensitive, sep = ",") %>% #separating all the species in the current assemblage column
+    group_by(cluster_name, current_assemblage_Flow_Sensitive) %>% #then regrouping them by cluster name
+    summarise(cluster_assemblage = paste0(current_assemblage_Flow_Sensitive, collapse=", "))
 
-central_valley <- filter(clustering_results, huc_region_group == 'central_valley') 
+  summarised <- separated %>% group_by(cluster_name) %>%
+          summarise(cluster_assemblage2 = paste0(current_assemblage_Flow_Sensitive, collapse = ", "))
 
-central_valley <- separate_rows(central_valley, current_assemblage_Flow_Sensitive, sep = ",") %>% #separating all the species in the current assemblage column
-  group_by(cluster_name) %>% #then regrouping them by cluster name
-  summarise(current_assemblage_Flow_Sensitive = paste0(unique(current_assemblage_Flow_Sensitive), collapse = ", "))
+  
+  #how to add this back to the original data frame?
+  #maybe create a new df and do a join based on cluster_name?
+  
+  return(summarised) #rename this so we can do a join
+}
 
-#how to add this back to the original data frame?
-#maybe create a new df and do a join based on cluster_name?
+#reshape_table <- function(clustering_data, region_group){
+#  filtered <- filter(clustering_results, huc_region_group == region_group)
+#  separated <- separate_rows(filtered, current_assemblage_Flow_Sensitive, sep = ",") %>% #separating all the species in the current assemblage column
+#    group_by(cluster_name) %>% #then regrouping them by cluster name
+#}
 
-central_valley <- rename(central_valley, Cluster_Assemblage = current_assemblage_Flow_Sensitive) #rename this so we can do a join
-
-###############################################################
-
-#repeat the above for the remaining 3 regions
-
-### great basin
-
-
-great_basin <- filter(clustering_results, huc_region_group == "great_basin")
-
-great_basin <- separate_rows(great_basin, current_assemblage_Flow_Sensitive, sep = ",") %>% #separating all the species in the current assemblage column
-  group_by(cluster_name) %>% #then regrouping them by cluster name
-  summarise(current_assemblage_Flow_Sensitive = paste0(unique(current_assemblage_Flow_Sensitive), collapse = ", "))
-
-#how to add this back to the original data frame?
-#maybe create a new df and do a join based on cluster_name?
-
-great_basin <- rename(great_basin, Cluster_Assemblage = current_assemblage_Flow_Sensitive) #rename this so we can do a join
-
-### north coast
-
-north_coast <- filter(clustering_results, huc_region_group == "north_coast")
-
-north_coast <- separate_rows(north_coast, current_assemblage_Flow_Sensitive, sep = ",") %>% #separating all the species in the current assemblage column
-  group_by(cluster_name) %>% #then regrouping them by cluster name
-  summarise(current_assemblage_Flow_Sensitive = paste0(unique(current_assemblage_Flow_Sensitive), collapse = ", "))
-
-#how to add this back to the original data frame?
-#maybe create a new df and do a join based on cluster_name?
-
-north_coast <- rename(north_coast, Cluster_Assemblage = current_assemblage_Flow_Sensitive) #rename this so we can do a join
-
-
-### south coast
-
-south_coast <- filter(clustering_results, huc_region_group == "south_coast")
-
-south_coast <- separate_rows(south_coast, current_assemblage_Flow_Sensitive, sep = ",") %>% #separating all the species in the current assemblage column
-  group_by(cluster_name) %>% #then regrouping them by cluster name
-  summarise(current_assemblage_Flow_Sensitive = paste0(unique(current_assemblage_Flow_Sensitive), collapse = ", "))
-
-#how to add this back to the original data frame?
-#maybe create a new df and do a join based on cluster_name?
-
-south_coast <- rename(south_coast, Cluster_Assemblage = current_assemblage_Flow_Sensitive) #rename this so we can do a join
+central_valley <- get_cluster_assemblages(clustering_results = clustering_data, region_group = "central_valley")
+great_basin <- get_cluster_assemblages(clustering_results = clustering_data, region_group = "great_basin")
+south_coast <- get_cluster_assemblages(clustering_results = clustering_data, region_group = "south_coast")
+north_coast <- get_cluster_assemblages(clustering_results = clustering_data, region_group = "north_coast")
 
 
 ## bind and join!!!
 
 cluster_assemblages <- bind_rows(central_valley, great_basin, north_coast, south_coast)
 
-final_results_cluster_assemblages <- full_join(clustering_results, cluster_assemblages, by = "cluster_name")
+final_results_cluster_assemblages <- full_join(clustering_data, cluster_assemblages, by = "cluster_name")
