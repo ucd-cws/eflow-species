80d4ae5ebdbf712d6047b0ec7ef3cddff340de09
Nick <nrsantos@ucdavis.edu>
2019-01-10 20:04:18 -0800

    Enhancements to flow class checker script to make it better handle branching and merging streams

--- check_flow_class.py
+++ check_flow_class.py
@@ -10,7 +10,7 @@ CHECK_FIELD = "check_status"
 CHECK_CODE_FIELD = "check_code"
 CHECK_DESCRIPTION_FIELD = "check_description"
 DOWNSTREAM_FIELD = "TOCOMID"  # needs to be configured to match the actual dataset - this must be joined in from NHDPlus
-DRAINAGE_AREA_FIELD = "TotDASqKm"  ## TODO: Make this the correct field name - filling in the NHDPlus default info for now
+DRAINAGE_AREA_FIELD = "DivDASqKM"  ## Using the divergence-routed upstream area field so that our percentage flow calculation works correctly
 MAJOR_TRIBUTARY_THRESHOLD = 0.25  # What proportion of downstream flow must a segment comprise before it's a major tributary to downstream
 
 flow_class_codes = {
@@ -56,15 +56,16 @@ class NHDSegment(object):
 	downstream_comid = None
 	flow_class = None
 	drainage_area = None
-	flag_status = []
-	flag_code = []
-	flag_description = []
 
 	def __init__(self, comid, downstream_comid, flow_class, drainage_area):
 		self.comid = comid
 		self.downstream_comid = downstream_comid
 		self.flow_class = flow_class
 		self.drainage_area = drainage_area
+		self.flag_status = []
+		self.flag_code = []
+		self.flag_description = []
+		self.upstream = []
 
 	@property
 	def downstream(self, comid=None):  # gets the downstream object if configured, or sets the downstream comid if comid is provided
@@ -72,7 +73,7 @@ class NHDSegment(object):
 			self.downstream_comid = comid
 		else:
 			if self.downstream_comid and self.downstream_comid in nhd_segments:
-				return nhd_segments[self.downstream_comid]
+				return nhd_segments[int(self.downstream_comid)]
 			elif not self.downstream_comid:
 				raise ValueError("Can't retrieve downstream object - not yet fully loaded and don't have its comid!")
 			else:
@@ -92,7 +93,17 @@ class NHDSegment(object):
 			return False
 
 
-if __name__ == "__main__":  # if this is the main script and hasn't been imported
+def build_network(segments):
+	for segment_id in segments:
+		segment = segments[segment_id]
+		try:
+			if segment not in segment.downstream.upstream:
+				segment.downstream.upstream.append(segment)
+		except ValueError:
+			continue  # we'll get that if the downstream doesn't exist - skip it
+
+
+def check_flow_classes():
 	print("Getting information about the input")
 	describer = arcpy.Describe(features_to_check)
 	field_names = [field.name for field in describer.fields]  # get just the names of the fields
@@ -108,15 +119,18 @@ if __name__ == "__main__":  # if this is the main script and hasn't been importe
 	if CHECK_CODE_FIELD not in field_names:
 			arcpy.AddField_management(features_to_check, CHECK_CODE_FIELD, "TEXT")
 	if CHECK_DESCRIPTION_FIELD not in field_names:
-			arcpy.AddField_management(features_to_check, CHECK_DESCRIPTION_FIELD, "TEXT")
+			arcpy.AddField_management(features_to_check, CHECK_DESCRIPTION_FIELD, "TEXT", field_length=8192)
 
 	data_input = arcpy.SearchCursor(features_to_check)
 
 	print("Reading Data")
 	# read the data in so we can arbitrarily jump around it
 	for row in data_input:
-		nhd_segments[row.getValue("COMID")] = NHDSegment(row.getValue("COMID"), row.getValue(DOWNSTREAM_FIELD), row.getValue(CLASS_FIELD))
+		nhd_segments[row.getValue("COMID")] = NHDSegment(row.getValue("COMID"), row.getValue(DOWNSTREAM_FIELD), row.getValue(CLASS_FIELD), row.getValue(DRAINAGE_AREA_FIELD))
 	del data_input  # clear out the cursor
+
+	print("Building Upstream Network")
+	build_network(nhd_segments)
 
 	# now we have all the data in a structure we can traverse
 
@@ -127,30 +141,39 @@ if __name__ == "__main__":  # if this is the main script and hasn't been importe
 		if checked_items % 10000 == 0:  # every time we evaluate 10k items
 			print(checked_items)
 
-		flow_class = nhd_segments[comid].flow_class
+		current_segment = nhd_segments[comid]
+		flow_class = current_segment.flow_class
 		try:
-			downstream_flow_class = nhd_segments[comid].downstream.flow_class
+			downstream_flow_class = current_segment.downstream.flow_class
 		except ValueError as e:
-			nhd_segments[comid].flag_status.append("flagged")
-			nhd_segments[comid].flag_code.append("network_issue")
-			nhd_segments[comid].flag_description.append("The status of this segment was unable to be evaluated due to coding or incorrect network information - error raised was \"{}\"".format(str(e)))
+			current_segment.flag_status.append("flagged")
+			current_segment.flag_code.append("network_issue")
+			current_segment.flag_description.append("The status of this segment was unable to be evaluated due to coding or incorrect network information - error raised was \"{}\"".format(str(e)))
 			continue
 
-		if not nhd_segments[comid].is_major_tributary:
-			if len(nhd_segments[comid].downstream.flag_status) == 0:  # only set the status if there *is* no status already - we'll likely overwrite it later when we get the major tributary
-				nhd_segments[comid].downstream.flag_status.append("skipped")  # set it to skipped so that we know if there was no major trib. If there is, it'll be overwritten. We're more likely to get two major tribs, probably
-				nhd_segments[comid].flag_code.append("skipped")
-				nhd_segments[comid].flag_description.append("skipped")
+		if not current_segment.is_major_tributary:
+			if len(current_segment.downstream.flag_status) == 0:  # only set the status if there *is* no status already - we'll likely overwrite it later when we get the major tributary
+				current_segment.downstream.flag_status.append("skipped")  # set it to skipped so that we know if there was no major trib. If there is, it'll be overwritten. We're more likely to get two major tribs, probably
+				current_segment.flag_code.append("skipped")
+				current_segment.flag_description.append("skipped")
 			continue
 
 		if not downstream_is_valid(upstream_class=flow_class, downstream_class=downstream_flow_class):
-			nhd_segments[comid].downstream.flag_status.append("flagged")
-			nhd_segments[comid].downstream.flag_code.append("suspect_flow_code")
-			nhd_segments[comid].downstream.flag_description.append("Flow class of {} is not in the accepted downstream flow classes for flow class {} from upstream COMID {}".format(downstream_flow_class, flow_class, comid))
-		else:
-			nhd_segments[comid].downstream.flag_status.append("checked")
-			nhd_segments[comid].flag_code.append("checked")
-			nhd_segments[comid].flag_description.append("checked")
+			# if the flow class is invalid, check if the downstream flow class is valid for *any* of the upstream items
+			for upstream in current_segment.downstream.upstream:
+				if upstream is current_segment:  # no need to recheck the current item, which will be the case most times
+					continue
+				if upstream.is_major_tributary and downstream_is_valid(upstream_class=upstream.flow_class, downstream_class=downstream_flow_class):
+					break  # if we find a valid upstream/downstream relationship, break out of this loop
+			else:  # otherwise, flag the downstream segment
+				current_segment.downstream.flag_status.append("flagged")
+				current_segment.downstream.flag_code.append("suspect_flow_code")
+				current_segment.downstream.flag_description.append("Flow class of {} is not in the accepted downstream flow classes for flow class {} from upstream COMID {}".format(downstream_flow_class, flow_class, comid))
+				continue  # continue here so that the "checked" below is skipped
+
+		current_segment.downstream.flag_status.append("checked")
+		current_segment.flag_code.append("checked")
+		current_segment.flag_description.append("checked")
 
 
 	print("Writing Results")
@@ -158,7 +181,17 @@ if __name__ == "__main__":  # if this is the main script and hasn't been importe
 	data_output = arcpy.UpdateCursor(features_to_check)
 	for row in data_output:
 		nhd_segment = nhd_segments[row.getValue("COMID")]
-		row.setValue(CHECK_FIELD, ",".join(nhd_segment.flag_status))
-		row.setValue(CHECK_CODE_FIELD, ",".join(nhd_segment.flag_code))
-		row.setValue(CHECK_DESCRIPTION_FIELD, ",".join(nhd_segment.flag_description))
-		data_output.updateRow(row)
+		try:
+			row.setValue(CHECK_FIELD, ",".join(nhd_segment.flag_status))
+			row.setValue(CHECK_CODE_FIELD, ",".join(nhd_segment.flag_code))
+			row.setValue(CHECK_DESCRIPTION_FIELD, ",".join(nhd_segment.flag_description))
+			data_output.updateRow(row)
+		except RuntimeError:
+			print("COMID: {}".format(comid))
+			print("CHECK_FIELD: {}".format(",".join(nhd_segment.flag_status)))
+			print("CHECK_CODE_FIELD: {}".format(",".join(nhd_segment.flag_code)))
+			print("CHECK_DESCRIPTION_FIELD: {}".format(",".join(nhd_segment.flag_description)))
+			raise
+
+if __name__ == "__main__":  # if this is the main script and hasn't been imported
+	check_flow_classes()
